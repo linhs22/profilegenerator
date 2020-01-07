639822fede7673a9973b0b4111ee454c2d64b598
Henry Lin <henrylin58@gmail.com>
2020-01-06 23:19:25 -0800

    completed functional portion of profile generator

--- index.js
+++ index.js
@@ -48,47 +48,100 @@ async function init() {
     try {
         const answers = await promptUser();
 
-        function generateHTML(userData) {
-            console.log(userData.profilePic)
-            return `<!DOCTYPE html>
-  <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <body>
-    <h1><img src=${userData.profilePic} alt="Smiley face" height="200" width="200"></h1>
-</body>`
-
-        }
+        function generateHTML(userData) {}
         const data = await gs(answers.username, function(err, data) {
+
             const userData = {
+                website: data.website,
+                name: data.name,
                 profilePic: data.avatar,
                 userid: data.username,
                 location: data.location,
-                github: `https://github.com/${data.username}`,
                 bio: data.bio,
                 repo: data.repos,
                 followers: data.followers,
                 stars: data.stars,
                 following: data.following
             }
+            const html = `<!DOCTYPE html>
+            <html lang="en">
+            
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
+                <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+                <title>Document</title>
+            </head>
+            
+            <body>
+                <div class="container">
+                    <h1><img src="https://avatars0.githubusercontent.com/u/55717746?s=400&v=4" alt="Smiley face" height="200" width="200"></h1>
+                    <h1>Hi!</h1>
+                    <h1>My name is ${userData.name}</h1>
+                    <h3>Currently ${userData.worksfor} </h3>
+                    <ul>
+                        <li><a href='https://www.google.com/maps/place/${userData.location}/'><i class=“fas fa-location-arrow”></i>${userData.location}</a></li>
+                        <li><a href=“https://github.com/${userData.userid}“><i class=“fas fa-location-arrow”></i>Github</a></li>
+                        <li><a href=“${userData.website}“><i class=“fas fa-location-arrow”></i>Website</a></li>
+                    </ul>
+                    <div class="row">
+                        ${userData.bio}
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                Public Repositories
+                            </div>
+                            <div class="row">
+                                ${userData.repo}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                Followers
+                            </div>
+                            <div class="row">
+                                ${userData.followers}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="row">
+                                Github Stars
+                            </div>
+                            <div class="row">
+                                ${userData.stars}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="row">
+                                Following
+                            </div>
+                            <div class="row">
+                                ${userData.following}
+                            </div>
+                        </div>
+                    </div>
+            
+                </div>
+            
+                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+            </body>
+            
+            </html>`
 
             generateHTML(userData);
+            writeFileAsync("index.html", html);
+
+            console.log("Successfully wrote to index.html");
         });
 
-
-
-
-
-
-
-        await writeFileAsync("index.html", generateHTML(userData));
-
-        console.log("Successfully wrote to index.html");
     } catch (err) {
         console.log(err);
     }
